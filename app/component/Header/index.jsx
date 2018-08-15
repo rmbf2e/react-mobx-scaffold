b51041d1098a7e717a4a5c2a65b84e36536838ed
superwf <superwf@gmail.com>
2018-08-15 11:14:11 +0800

    fix: 修正Header代码删除无用部分

--- app/component/Header/index.jsx
+++ app/component/Header/index.jsx
@@ -1,35 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { observer, inject } from 'mobx-react'
 import ShareHeader from 'share/component/Header'
 import Menu from 'share/component/Menu'
 import s from './style.m.less'
 
-// const { Option } = Select
-
-@inject('store')
-@observer
-export default class AppHeader extends React.Component {
-  static propTypes = {
-    store: PropTypes.shape({
-      router: PropTypes.shape({
-        location: PropTypes.object,
-      }),
-    }).isRequired,
-  }
-
-  // 通过路由获取当前页面名称
-
-  render() {
-    const { store } = this.props
-    return (
-      <ShareHeader>
-        <figure className={s.logo}>
-          <img alt="logo" src="/asset/image/logo.png" />
-          站点标题
-        </figure>
-        <Menu />
-      </ShareHeader>
-    )
-  }
-}
+const AppHeader = () => (
+  <ShareHeader>
+    <figure className={s.logo}>
+      <img alt="logo" src="/asset/image/logo.png" />
+      站点标题
+    </figure>
+    <Menu />
+  </ShareHeader>
+)
+export default AppHeader
