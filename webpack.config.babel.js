86053ffdf21f082460820ec5e9e6dab3987eef0b
superwf <superwf@gmail.com>
2018-10-11 16:21:17 +0800

    refactor(webpack.config): mv sw-plugin to production env

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -246,32 +246,6 @@ config.plugins = [
   }),
   // 可以优化不需要的额外moment的语言包
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  new SWPrecacheWebpackPlugin({
-    // By default, a cache-busting query parameter is appended to requests
-    // used to populate the caches, to ensure the responses are fresh.
-    // If a URL is already hashed by Webpack, then there is no concern
-    // about it being stale, and the cache-busting can be skipped.
-    dontCacheBustUrlsMatching: /\.\w{8}\./,
-    filename: 'service-worker.js',
-    logger(message) {
-      if (message.indexOf('Total precache size is') === 0) {
-        // This message occurs for every build and is a bit too noisy.
-        return
-      }
-      if (message.indexOf('Skipping static resource') === 0) {
-        // This message obscures real errors so we ignore it.
-        // https://github.com/facebookincubator/create-react-app/issues/2612
-      }
-    },
-    minify: true,
-    // For unknown URLs, fallback to the index page
-    navigateFallback: `/index.html`,
-    // Ignores URLs starting from /__ (useful for Firebase):
-    // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-    navigateFallbackWhitelist: [/^(?!\/__).*/],
-    // Don't precache sourcemaps (they're large) and build asset manifest:
-    staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-  }),
 ]
 
 if (isProd) {
@@ -303,6 +277,32 @@ if (isProd) {
       threshold: 2048,
     }),
     // new BundleAnalyzerPlugin(),
+    new SWPrecacheWebpackPlugin({
+      // By default, a cache-busting query parameter is appended to requests
+      // used to populate the caches, to ensure the responses are fresh.
+      // If a URL is already hashed by Webpack, then there is no concern
+      // about it being stale, and the cache-busting can be skipped.
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          // This message occurs for every build and is a bit too noisy.
+          return
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          // This message obscures real errors so we ignore it.
+          // https://github.com/facebookincubator/create-react-app/issues/2612
+        }
+      },
+      minify: true,
+      // For unknown URLs, fallback to the index page
+      navigateFallback: `/index.html`,
+      // Ignores URLs starting from /__ (useful for Firebase):
+      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      // Don't precache sourcemaps (they're large) and build asset manifest:
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
   )
   config.optimization = {
     splitChunks: {
